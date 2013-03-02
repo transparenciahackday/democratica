3205aca6d3a6daa1956de4ea7e58a10fd3d7c413
Ricardo Lafuente <r@sollec.org>
2013-03-02 19:35:39 +0000

    Tastypie mal copiado no último commit, corrigi

--- tastypie/tests/content_gfk/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-import warnings
-warnings.simplefilter('ignore', Warning)
-
-from content_gfk.tests.fields import *
-from content_gfk.tests.resources import *
--- tastypie/tests/settings_content_gfk.py
+++
@@ -1,7 +0,0 @@
-from settings import *
-
-INSTALLED_APPS += [
-    'content_gfk',
-]
-
-ROOT_URLCONF = 'content_gfk.urls'
