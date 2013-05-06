afb5aad1c65d45c6d6d0ee27826a8391598afbc8
Ricardo Lafuente <r@sollec.org>
2013-05-06 20:09:21 +0100

    Pepoitização dos replaces

--- REPLACES_DEPUTADOS.py
+++ REPLACES_DEPUTADOS.py
@@ -6,35 +6,32 @@
 # Lurdes Ribeiro
 # Pedro Silva Marques
 # Juvenal Silva Peneda
-# Filipe Lobo d'Ávila
 
 # Rui Barreto - PARLAMENTO?
 # 100: Presidente da República
 # 012, 032, 043: verificar documentos, speakers errados
 
 
-
 SHORTNAME_REPLACES = {
-        u'António Amaro': u'António Leitão Amaro',
-        u'Maria Gabriela Canavilhas': u'Gabriela Canavilhas',
-        u'José Matos Rosa': u'José de Matos Rosa',
-        u'José Matos Correia': u'José de Matos Correia',
-        u'Maria da Conceição Pereira': u'Maria Conceição Pereira',
-        u'Maria Helena André': u'Helena André',
-        u'Duarte Marques': u'Duarte Filipe Marques',
-        u'Maria Francisca Almeida': u'Francisca Almeida',
-        u'Francisco Assis': u'Francisco de Assis',
-        u'Paulo Baptista Santos': u'Paulo Batista Santos',
-        u'João Antunes': u'João Figueiredo',
-        u'Carlos Gonçalves': u'Carlos Alberto Gonçalves',
-        u'Ana Catarina Mendes': u'Ana Catarina Mendonça',
-        u'António Peixoto': u'Carlos Peixoto',
-        u'Carlos Abreu Amorim': u'Fialho Isaac',
-        u'João Mota Amaral': u'Mota Amaral',
-        u'Teresa Costa Santos': u'Teresa Santos',
-        u'Isabel Moreira': u'Isabel Alves Moreira',
-        u'Maria das Mercês Borges': u'Maria das Mercês Soares',
-        u'Maria Antónia de Almeida Santos': u'Maria Antónia Almeida Santos',
-        u"Filipe Lobo D' Ávila": u"Filipe Lobo d'Ávila",
-        }
-
+    u'António Amaro': u'António Leitão Amaro',
+    u'Maria Gabriela Canavilhas': u'Gabriela Canavilhas',
+    u'José Matos Rosa': u'José de Matos Rosa',
+    u'José Matos Correia': u'José de Matos Correia',
+    u'Maria da Conceição Pereira': u'Maria Conceição Pereira',
+    u'Maria Helena André': u'Helena André',
+    u'Duarte Marques': u'Duarte Filipe Marques',
+    u'Maria Francisca Almeida': u'Francisca Almeida',
+    u'Francisco Assis': u'Francisco de Assis',
+    u'Paulo Baptista Santos': u'Paulo Batista Santos',
+    u'João Antunes': u'João Figueiredo',
+    u'Carlos Gonçalves': u'Carlos Alberto Gonçalves',
+    u'Ana Catarina Mendes': u'Ana Catarina Mendonça',
+    u'António Peixoto': u'Carlos Peixoto',
+    u'Carlos Abreu Amorim': u'Fialho Isaac',
+    u'João Mota Amaral': u'Mota Amaral',
+    u'Teresa Costa Santos': u'Teresa Santos',
+    u'Isabel Moreira': u'Isabel Alves Moreira',
+    u'Maria das Mercês Borges': u'Maria das Mercês Soares',
+    u'Maria Antónia de Almeida Santos': u'Maria Antónia Almeida Santos',
+    u"Filipe Lobo D' Ávila": u"Filipe Lobo d'Ávila",
+}
